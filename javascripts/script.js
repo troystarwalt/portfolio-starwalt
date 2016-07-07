1c6a99bc31493551655281c5442110897651329e
Troy Starwalt <troy.starwalt@gmail.com>
2016-07-07 13:31:33 -0400

    Updated portfolio

--- javascripts/script.js
+++ javascripts/script.js
@@ -8,6 +8,9 @@ $(document).ready(function() {
 	$("#jenny-more").click(function() {
 		$("#jenny-info").toggle("drop", {direction: "up"});
 	});
+	$("#xaxis-more").click(function() {
+		$("#xaxis-info").toggle("drop", {direction: "up"});
+	});
 
 	// Below is code that replaces #who-am-i with items from the array on a loop.
 
